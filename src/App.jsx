58cf8eddbc80348b604ad79dc7e7b301ac6b0bef
Tzuyu <manekete@gmail.com>
2023-07-21 00:18:36 -0600

    Add AppLayout component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import Order from './features/order/Order';
+import CreateOrder from './features/order/CreateOrder';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
+
+// goto: 7 Fetching Data
